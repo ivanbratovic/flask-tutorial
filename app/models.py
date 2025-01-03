dea4858725f04460f5281bda94ff68be5d3d92b5
Ivan Bratović <ibratovic@neogov.net>
2025-01-03 15:07:32 +0100

    Chapter 5

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -13,8 +15,19 @@ class User(db.Model):
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 
 class Post(db.Model):
