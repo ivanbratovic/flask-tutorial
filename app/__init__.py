31b52779b51b981255ffc67d9287dcdb045dd28e
Ivan Bratović <ibratovic@neogov.net>
2025-01-07 16:21:19 +0100

    Chapter 12

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
@@ -14,6 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+moment = Moment(app)
+
 login.login_view = "login"
 
 if not app.debug:
